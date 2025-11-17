757b995aa810acfc5da6efb2502b58002189749a
Susandeep Shakya <susandeep.shakya@frostdigi.ai>
2025-11-17 22:23:33 +0545

    Feat: Added Unit testing using React test library to test components

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  // @ts-ignore - Vitest config extension
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
 })
